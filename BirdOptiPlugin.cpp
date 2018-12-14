ea8b975b10ef1e5c81aa6f68eb10d591979f93b8
strangechu <strangechu@gmail.com>
2018-12-14 21:28:36 +0900

    x64 release for dll

---
+++ BirdOptiPlugin.cpp
@@ -0,0 +1,6 @@
+#include "BirdOptiPlugin.h"
+
+__declspec(dllexport) int __stdcall Add(int a, int b)
+{
+	return a + b;
+}
---
+++ BirdOptiPlugin.h
@@ -0,0 +1,1 @@
+extern "C" __declspec(dllexport) int __stdcall Add(int a, int b);
